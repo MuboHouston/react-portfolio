8111988f014215fde332fa7279f547126e650348
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-08 16:21:03 -0500

    add Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <header>
+            <h2>
+                <a href='/'>
+                    Mubo Houston
+                </a>
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        About Me
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Resume
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
