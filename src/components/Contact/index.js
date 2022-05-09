1a32825e5163be06a4689d4e7b523c75aae616ad
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-09 12:14:18 -0500

    add validation to form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers'
 
 function ContactForm() {
-    //sets the initial state to empty strings
+    //errorMessage hook- the initial error message is an empty string
+    const[errorMessage, setErrorMessage] = useState('')
+
+    //formState hook- sets the initial state to empty strings
     const [ formState, setFormState ] = useState({ name: '', email: '', message: ''});
 
     const { name, email, message } = formState;
     
     //syncing the internal state of the component formState with the user input from the DOM. 
     function handleChange(e){
-        //the name property of target refers to the name attribute of the form element
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid)
+
+            //isValid conditional statement 
+            if(!isValid) {
+                setErrorMessage('Your email is invalid')
+            } else {
+                setErrorMessage('')
+            }
+        } else {
+            //no validation needed for message or name inputs, so just checks that something was inputted in those two fields. If not, then an error message will appear
+            if(!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`)
+            } else {
+                setErrorMessage('')
+            }
+            console.log('errorMessage', errorMessage);
+        }
+
+        //wrapped in a conditional, so that the state only updates if the form data passed the quality tests
+        if(!errorMessage) {
+            //the name property of target refers to the name attribute of the form element
+            setFormState({...formState, [e.target.name]: e.target.value})
+        }
     }
     
     console.log(formState)
@@ -26,15 +53,20 @@ function ContactForm() {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
                 </div>
                 <button type="submit">Submit</button>
             </form>
